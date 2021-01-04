8dbc51bf25a277c6733315f13419addfb4f171d9
Anh Hao <hi@anhhao.me>
2021-01-04 15:29:47 +0700

    Update run

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@ cd "$(dirname "$0")"
 source venv/bin/activate
 python main.py
 cp wallpaper.png /home/anhhao/Pictures/Wallpapers/wallpaper.png
+now=$(date +"%T")
+echo "Wallpaper changed at: $now" >> log.tmp
